9ea185598b0d97467e8cfb2d31221e1288159935
Savannah Smith <savsmith@athenahealth.com>
2022-02-27 14:15:38 -0600

    fixed bug adding tests

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,9 +1,15 @@
 import * as assert from 'assert';
+import { SourceCode } from 'eslint';
+
+const testFolder = "../../../test_data";
+import fs = require('fs');
+import path = require('path');
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
+import * as vdiff from '../../extension';
 
 suite('Extension Test Suite', () => {
     vscode.window.showInformationMessage('Start all tests.');
@@ -11,5 +17,14 @@ suite('Extension Test Suite', () => {
     test('Sample test', () => {
         assert.strictEqual(-1, [1, 2, 3].indexOf(5));
         assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+    });
+
+    const testFolderPath = path.join(__dirname, testFolder);
+    const tests = fs.readdirSync(testFolderPath);
+    tests.forEach(test_folder => {
+        test('File Is Diffable', async () => {
+            const uri = path.join(testFolderPath, test_folder, "in.t");
+            await vscode.commands.executeCommand("vdiff.compareSelectedFileVersions", uri);
+        });
     });
 });
--- src/test/suite/regexUtil.test.ts
+++
@@ -1,6 +0,0 @@
-import * as RegexUtil from "../../comparer/regex-util"; 
-import * as assert from 'assert';
-
-suite('Extension Test Suite', () => {
-    throw new Error("NOT implemented yet");
-});
